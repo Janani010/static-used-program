bce2a9287e7305283d1bc45eb218420536f004be
Janani010 <113532819+Janani010@users.noreply.github.com>
2022-10-07 11:19:24 +0530

    Add files via upload

---
+++ Javaka.java
@@ -0,0 +1,26 @@
+class Javaka{
+
+static String course="Java";
+static String course2="python";
+
+
+static void madhavi(){
+System.out.println(course);
+}
+
+static void janani()
+{
+System.out.println(course2);
+}
+
+public static void main(String args[])
+{
+
+Javaka.madhavi();
+Javaka.janani();
+
+}
+}
+
+
+
